43b82c7d6b40990510bf106b3c24aca95b3c0327
gflahaut <75680769+gflahaut@users.noreply.github.com>
2021-07-17 21:24:15 +0100

    Add files via upload

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
 
   createUser(email: string, password: string) {
     return new Promise((resolve, reject) => {
-      this.http.post(baseUrl + '/api/auth/signup', { email: email, password: password }).subscribe(
+      this.http.post(baseUrl + 'api/auth/signup', { email: email, password: password }).subscribe(
         (response: { message: string }) => {
           resolve(response);
         },
