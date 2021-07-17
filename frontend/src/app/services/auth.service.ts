b6185e3ed1958c974b0bf8d61646ba252d8d1294
unknown <flahaut.gwendoline@gmail.com>
2021-07-18 00:49:26 +0100

    1

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
+const baseUrl = 'https://sopeckocko62.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +12,12 @@ export class AuthService {
   isAuth$ = new BehaviorSubject<boolean>(false);
   private authToken: string;
   private userId: string;
-
   constructor(private http: HttpClient,
-              private router: Router) {}
+    private router: Router) { }
 
   createUser(email: string, password: string) {
     return new Promise((resolve, reject) => {
-      this.http.post('http://localhost:3000/api/auth/signup', {email: email, password: password}).subscribe(
+      this.http.post(baseUrl + 'api/auth/signup', { email: email, password: password }).subscribe(
         (response: { message: string }) => {
           resolve(response);
         },
@@ -37,9 +37,9 @@ export class AuthService {
   }
 
   loginUser(email: string, password) {
-    return new Promise((resolve, reject) => {
-      this.http.post('http://localhost:3000/api/auth/login', {email: email, password: password}).subscribe(
-        (response: {userId: string, token: string}) => {
+    return new Promise<void>((resolve, reject) => {
+      this.http.post(baseUrl + '/api/auth/login', { email: email, password: password }).subscribe(
+        (response: { userId: string, token: string }) => {
           this.userId = response.userId;
           this.authToken = response.token;
           this.isAuth$.next(true);
--- frontend/src/app/services/sauces.service.ts
+++ frontend/src/app/services/sauces.service.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs';
 import { Sauce } from '../models/Sauce.model';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth.service';
+const baseUrl= 'https://sopeckocko62.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
@@ -87,7 +88,7 @@ export class SaucesService {
               private auth: AuthService) {}
 
   getSauces() {
-    this.http.get('http://localhost:3000/api/sauces').subscribe(
+    this.http.get(baseUrl+'/api/sauces').subscribe(
       (sauces: Sauce[]) => {
         this.sauces$.next(sauces);
       },
@@ -100,7 +101,7 @@ export class SaucesService {
 
   getSauceById(id: string) {
     return new Promise((resolve, reject) => {
-      this.http.get('http://localhost:3000/api/sauces/' + id).subscribe(
+      this.http.get(baseUrl+'/api/sauces/' + id).subscribe(
         (sauce: Sauce) => {
           resolve(sauce);
         },
@@ -113,8 +114,8 @@ export class SaucesService {
 
   likeSauce(id: string, like: boolean) {
     return new Promise((resolve, reject) => {
-      this.http.post(
-        'http://localhost:3000/api/sauces/' + id + '/like',
+      this.http.post(baseUrl+
+        '/api/sauces/' + id + '/like',
         {
           userId: this.auth.getUserId(),
           like: like ? 1 : 0
@@ -133,7 +134,7 @@ export class SaucesService {
   dislikeSauce(id: string, dislike: boolean) {
     return new Promise((resolve, reject) => {
       this.http.post(
-        'http://localhost:3000/api/sauces/' + id + '/like',
+        baseUrl+'/api/sauces/' + id + '/like',
         {
           userId: this.auth.getUserId(),
           like: dislike ? -1 : 0
@@ -154,7 +155,7 @@ export class SaucesService {
       const formData = new FormData();
       formData.append('sauce', JSON.stringify(sauce));
       formData.append('image', image);
-      this.http.post('http://localhost:3000/api/sauces', formData).subscribe(
+      this.http.post(baseUrl+'/api/sauces', formData).subscribe(
         (response: { message: string }) => {
           resolve(response);
         },
@@ -168,7 +169,7 @@ export class SaucesService {
   modifySauce(id: string, sauce: Sauce, image: string | File) {
     return new Promise((resolve, reject) => {
       if (typeof image === 'string') {
-        this.http.put('http://localhost:3000/api/sauces/' + id, sauce).subscribe(
+        this.http.put(baseUrl+'/api/sauces/' + id, sauce).subscribe(
           (response: { message: string }) => {
             resolve(response);
           },
@@ -180,7 +181,7 @@ export class SaucesService {
         const formData = new FormData();
         formData.append('sauce', JSON.stringify(sauce));
         formData.append('image', image);
-        this.http.put('http://localhost:3000/api/sauces/' + id, formData).subscribe(
+        this.http.put(baseUrl+'/api/sauces/' + id, formData).subscribe(
           (response: { message: string }) => {
             resolve(response);
           },
@@ -194,7 +195,7 @@ export class SaucesService {
 
   deleteSauce(id: string) {
     return new Promise((resolve, reject) => {
-      this.http.delete('http://localhost:3000/api/sauces/' + id).subscribe(
+      this.http.delete(baseUrl+'/api/sauces/' + id).subscribe(
         (response: { message: string }) => {
           resolve(response);
         },
